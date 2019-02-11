dd88a04e0c32472b2cd3021e928e48867f0bd4b7
ANIMESH KUMAR <animeshk@webelight.com>
2019-02-11 14:39:41 +0530

    Added codes for subscrribe and unsubscribe method

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,14 +1,16 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action ) => {
+const store = createStore((state = { count: 5 }, action ) => {
   switch(action.type) {
     case 'INCREMENT':
+    const addByincrement = typeof action.addByincrement === 'number' ? action.addByincrement : 1;
     return {
-      count: state.count + 1
+      count: state.count + addByincrement
     }
     case 'DECREMENT':
+    const decrementNumberBy = typeof action.decrementNumberBy === 'number' ? action.decrementNumberBy : -1;
     return {
-      count: state.count - 1
+      count: state.count - decrementNumberBy
     }
     case 'RESET':
     return {
@@ -19,24 +21,44 @@ const store = createStore((state = { count: 0 }, action ) => {
   }
 });
 
-console.log(store.getState());
+
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+  addByincrement: 5
+});
+
+// unsubscribe ();
 
 store.dispatch({
   type: 'INCREMENT'
 });
 
-store.dispatch({
-  type: 'DECREMENT'
-});
-store.dispatch({
-  type: 'INCREMENT'
-});
 store.dispatch({
   type: 'RESET'
 });
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'DECREMENT',
+  decrementNumberBy: 9999
+});
+store.dispatch({
+  type: 'INCREMENT',
+  addByincrement: 5
+});
+store.dispatch({
+  type: 'INCREMENT',
+  addByincrement: 10
+});
+store.dispatch({
+  type: 'INCREMENT',
+  addByincrement: 5
 });
 
-
-console.log(store.getState());

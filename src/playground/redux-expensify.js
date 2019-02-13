d716e64c4f937d316d9317a27afa1cabcf6de22e
ANIMESH KUMAR <animeshk@webelight.com>
2019-02-13 19:02:01 +0530

    store.dispatch(setStartDate(-10000025));      // startDate 125 ::::::::: and :::::::::: store.dispatch(setStartDate(10000025));      // startDate 125::::::::why is output giving like [{…}]

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -18,11 +18,46 @@ const addExpense = ({
   }
 });
 
-//REmove Expense 
+//Remove Expense
 const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
-})
+});
+
+// Edit Expense
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
+// SORT_BY_DATE
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+// SORT_BY_AMOUNT
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+// SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+// SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
+// Expenses Reducer
 
 const expensesReducerDefaultState = [];
 
@@ -35,6 +70,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       ];
       case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id);
+      case 'EDIT_EXPENSE':
+          return state.map((expense) => {
+              if (expense.id === action.id) {
+                  return {
+                      ...expense,
+                      ...action.updates
+                  };
+              } else {
+                  return expense;
+              }
+          });
     default:
       return state;
   }
@@ -48,37 +94,89 @@ const filtersReducerDefaultState = {
   endDate: undefined
 };
 
-const filterReducer = (state = filtersReducerDefaultState, action) => {
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+      case 'SET_TEXT_FILTER':
+          return {
+              ...state,
+              text: action.text
+          };
+      case 'SORT_BY_AMOUNT':
+              return {
+                  ...state,
+                  sortBy: 'amount'
+              };
+      case 'SORT_BY_DATE':
+          return {
+              ...state,
+              sortBy: 'date'
+          };
+      case 'SET_START_DATE':
+          return {
+              ...state,
+              startDate: action.startDate
+          };
+      case 'SET_END_DATE':
+          return {
+              ...state,
+              endDate: action.endDate
+          };
     default:
       return state;
   }
+};
+//timestamps (milliseconds
+//January 1st 1970 (unix approach)
+// 33400, 10, -233
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMarch = typeof  startDate !== 'number' || expense.createAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createAt <= endDate;
+        const textMatch = true;
+
+        return startDateMarch && endDateMatch && textMatch;
+    });
 };
 
 // store creation
 const store = createStore(
   combineReducers({
     expenses: expensesReducer,
-    filters: filterReducer
+    filters: filtersReducer
   })
 );
 
 store.subscribe(() => {
-    console.log("inside subscribe method",store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+    // console.log("inside subscribe method",store.getState());
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 55 }));
-const expenseThree = store.dispatch(addExpense({ description: 'Biryani', amount: 156 }));
-const expenseFour = store.dispatch(addExpense({ description: 'water', amount: 10 }));
-const expenseFive = store.dispatch(addExpense({ description: 'Room', amount: 1000, createAt: 45 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createAt: 5000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 55, createAt: -1000 }));
+// // const expenseThree = store.dispatch(addExpense({ description: 'Biryani', amount: 156 }));
+// // const expenseFour = store.dispatch(addExpense({ description: 'water', amount: 10 }));
+// // const expenseFive = store.dispatch(addExpense({ description: 'Room', amount: 1000, createAt: 45 }));
+//
+// store.dispatch(removeExpense({ id: expenseOne.expense.id}));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500, createAt: 4545 }));
+//
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+//
+// store.dispatch(sortByAmount()); // amount
+// store.dispatch(sortByDate()); // date
+// // console.log("GET Object list so that we can edit it", expenseOne)
+//
+store.dispatch(setStartDate(-10000025));      // startDate 125
+// store.dispatch(setStartDate()); // startDAte undefined
+// store.dispatch(setEndDate(1250)); // endDate 1250
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id}));
 
-console.log("GET Object list so that we can edit it", expenseOne)
-
-
-console.log("outsidesubscribe method",store.getState());
+// console.log("outsidesubscribe method",store.getState());
 
 const demoState = {
   expenses: [
@@ -87,7 +185,7 @@ const demoState = {
       description: "January Rent",
       note: "This was the final payment for that adress",
       amount: 54500,
-      createdAt: 0
+      createdAt: 250
     }
   ],
   filters: {
@@ -99,11 +197,11 @@ const demoState = {
   }
 };
 
-console.log("expensify");
-
-
-const user = {
-    name: 'Jen',
-    age: 24
-}
-console.log('testing in console the spread operator values',{age: 29, ...user, name: 'Animesh',location: 'India'});
+// console.log("expensify");
+//
+//
+// const user = {
+//     name: 'Jen',
+//     age: 24
+// }
+// console.log('testing in console the spread operator values',{age: 29, ...user, name: 'Animesh',location: 'India'});
